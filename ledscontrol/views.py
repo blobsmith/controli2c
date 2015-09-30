8a341ca7a13764b552a91038e32aa49f7987f8b0
ogilbert <blobus2@hotmail.com>
2015-09-30 15:23:50 +0200

    Active I2C Bus and disabled Django CSRF

--- ledscontrol/views.py
+++ ledscontrol/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from ledscontrol.extras import leds
+from django.views.decorators.csrf import csrf_exempt
 
 
 # i2c bus for raspberry pi (model B) on port 1
-# from smbus import SMBus
-# bus = SMBus(1)
+from smbus import SMBus
+bus = SMBus(1)
 
 arduinoAddress = 0x12
 value = 0b00000
@@ -16,11 +17,8 @@ leds.asByte = int(value)
 def view_test(request):
 	return render(request, 'ledscontrol/test.html', {'leds': leds})
 
+@csrf_exempt
 def view_update(request):
-
-	# i2c bus for raspberry pi on port 1
-	# bus = smbus.SMBus(1)
-
 	data = {}
 	data["ref"] = request.POST.get("ref", None)
 	data["active"] = request.POST.get("active", None)
@@ -31,7 +29,7 @@ def view_update(request):
 	if data["ref"]:
 		setattr(leds, 'led'+data["ref"], int(data["active"]))
 
-	# bus.write_byte(arduinoAddress, leds.asByte)
+	bus.write_byte(arduinoAddress, leds.asByte)
 
 	data['result'] = 'success'
 	data['leds'] = {
